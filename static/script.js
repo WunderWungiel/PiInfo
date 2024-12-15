f2f7d88dc8b59d648d95f7178520eff3365c3dd7
Wunder Wungiel <dredlok706@gmail.com>
2024-12-15 20:22:35 +0100

    Migrate to Bootstrap & jQuery, add beautiful Material & Font Awesome icons, animations, and charts

--- static/script.js
+++ static/script.js
@@ -1,55 +1,91 @@
 function set_value(name) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText)
-            document.getElementById(name).textContent = json[name];
-        }
-    }
-    xhr.open("GET", `/api/get_${name}`, true);
-    xhr.send();
+    $.get(`/api/get_${name}`, function (json) {
+        $("#" + name).text(json[name]);
+    })
 }
 
-function set_ram() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText)
-            document.getElementById("ram_used").textContent = json["used"];
-            document.getElementById("ram_used_percent").textContent = json["used_percent"];
-            document.getElementById("ram_free").textContent = json["free"];
-            document.getElementById("ram_free_percent").textContent = json["free_percent"];
-            document.getElementById("ram_total").textContent = json["total"];
+function set_fan() {
+    $.get(`/api/get_fan_speed`, function (json) {
+        const fan_speed = json["fan_speed"];
+        $("#" + "fan_speed").text(fan_speed);
+        (fan_speed == 0) ? $("#fanIcon").removeClass("spin") : $("#fanIcon").addClass("spin");
+    })
+}
+
+function set_temperature() {
+    $.get(`/api/get_temperature`, function (json) {
+        let iconClass;
+        const temperature = json["temperature"];
+        $("#temperature").text(temperature.toString());
+        if (temperature >= 70) {
+            iconClass = "bi-thermometer-high";
+        } else if (temperature >= 50) {
+            iconClass = "bi-thermometer-half";
+        } else {
+            iconClass = "bi-thermometer-low";
         }
-    }
-    xhr.open("GET", "/api/get_ram", true);
-    xhr.send();
+        $("#temperatureIcon").removeClass();
+        $("#temperatureIcon").addClass(iconClass);
+    })
+}
+
+function set_ram(chart) {
+    $.get("/api/get_ram", function (json) {
+        chart.data.datasets[0].data = [json['used'], json['free']];
+        chart.update();
+    })
+}
+
+function set_cpu(chart) {
+    $.get("/api/get_cpu_usage", function (json) {
+        chart.data.datasets[0].data = [json['cpu_usage'], 100 - json["cpu_usage"]];
+        chart.update();
+    })
 }
 
 function set_disks() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-            var disks_div = document.getElementById("disks");
-            var html = "";
-            for (var key in json) {
-                html += `<p>${key} (mounted at ${json[key]['mountpoint']}): ${json[key]['used']} GB used (${json[key]['used_percent']}%) / ${json[key]['free']} GB free (${json[key]['free_percent']}%) / ${json[key]['total']} GB in total (100%)</p>`;
-            }
-            disks_div.innerHTML = html;
-
+    
+    $.get("/api/get_disks", function (json) {
+        let html = "";
+        for (const key in json) {
+            html += `<p>${key} (mounted at ${json[key]['mountpoint']}): ${json[key]['used']} GB used (${json[key]['used_percent']}%) / ${json[key]['free']} GB free (${json[key]['free_percent']}%) / ${json[key]['total']} GB in total (100%)</p>`;
         }
-    }
-    xhr.open("GET", "/api/get_disks", true);
-    xhr.send();
+        $("#disks").html(html);
+    })
 }
 
+const set_chart = (name, title) => {
+    const ctx = document.getElementById(`${name}_chart`);
+    const chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Used', 'Free'],
+            datasets: [{
+                label: title,
+                data: [0, 0],
+                backgroundColor: [
+                    "blue", "white"
+                ],
+                hoverOffset: 4
+            }]
+        },         
+        cutout: "70%"
+    })
+    return chart;
+} 
+
+Chart.overrides["doughnut"].plugins.legend.display = false;
+const ram_chart = set_chart("ram", "RAM")
+ram_chart.options.plugins.tooltip.callbacks.label = (context) => { return ` ${context.raw} GB` };
+const cpu_chart = set_chart("cpu", "CPU")
+cpu_chart.options.plugins.tooltip.callbacks.label = (context) => { return ` ${context.raw}%` };
+
 function set_all_items() {
-    set_value("temperature");
+    set_temperature();
     set_value("clock");
     set_value("fan_speed");
-    set_ram();
-    set_value("cpu_usage");
+    set_ram(ram_chart);
+    set_cpu(cpu_chart);
     set_disks();
 }
 
